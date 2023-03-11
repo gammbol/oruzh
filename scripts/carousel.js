94a9ff16759e197c4ea626c8c9b65af467a33f01
gammbol <71922180+gammbol@users.noreply.github.com>
2023-03-11 11:39:17 +0600

    finished exp section

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,23 +1,15 @@
-const slides = document.querySelectorAll('.expImg');
+const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
+let cur_slide = 1;
 
-slides.forEach((slide, index) => {
-  console.log(index);
-  slide.style.transform = `translateX(calc(${index * 100}% + ${300 * index}px)`;
+let slider = new KeenSlider('#my-keen-slider', {
+	centered: true
 });
 
-let curSlide = 0;
-let maxSlide = slides.length  - 1;
-const nextSlide = document.querySelector('.btnNext');
-const prevSlide = document.querySelector('.btnPrev');
+btnNext.addEventListener('click', () => {
+	slider.next();
+});
 
-nextSlide.addEventListener('click', () => {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(calc(${100 * (index - curSlide)}% - 400px))`;
-  });
+btnPrev.addEventListener('click', () => {
+	slider.prev();
 });
