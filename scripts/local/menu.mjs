cd0ab8a2d558b0fe1a185989b55b10353798c75c
ereburg <gimmethenick@gmail.com>
2023-03-19 16:06:15 +0300

    merge changes for header and first screen

--- scripts/local/menu.mjs
+++ scripts/local/menu.mjs
@@ -1,23 +1,16 @@
 export const MobileMenu = () => {
-  const $openBtn = document.querySelector('.headerMobileWrapper')
-  const $closeBtn = document.querySelector('.closeWrapper')
-  const $links = document.querySelectorAll('.mobileMenuItem a')
-  const $menuContainer = document.querySelector('.mobileMenuContainer')
-
-  let isOpen = false
+  const $openBtn = document.querySelector('.js-header-open')
+  const $closeBtn = document.querySelector('.js-header-close')
+  const $menuContainer = document.querySelector('.js-header-nav')
+  const $links = $menuContainer.querySelectorAll('a')
 
   const onToggle = () => {
-    if (isOpen) {
-      $menuContainer.style.top = '-100%'
-      isOpen = !isOpen
-    } else {
-      $menuContainer.style.top = '0'
-      isOpen = !isOpen
-    }
+    $menuContainer.classList.contains('-translate-y-full')
+      ? $menuContainer.classList.remove('-translate-y-full')
+      : $menuContainer.classList.add('-translate-y-full')
   }
 
   $openBtn.addEventListener('click', onToggle)
   $closeBtn.addEventListener('click', onToggle)
-
   $links.forEach((link) => link.addEventListener('click', onToggle))
 }
